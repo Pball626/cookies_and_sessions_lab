25c462402f46133c4642c4b80460ee70dcb70a1e
joey <pball626@gmail.com>
2021-03-29 13:17:57 -0500

    commit tree 7bfa37a0a56d2a6bf716b0800b424b71a097c270

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,18 @@
+class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all
+    end
+
+    def add_to_cart
+        # Get the item from the path
+        @product = Product.find(params[:id])
+      
+        # Load the cart from the session, or create a new empty cart.
+        cart = session[:cart] || []
+        cart << @product.id
+      
+        # Save the cart in the session.
+        session[:cart] = cart
+      end 
+end
